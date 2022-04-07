a8c964a90a4dbc3e9ea84d673aace4e5075a87e2
g20karjo <g20karjo@student.his.se>
2022-04-07 09:46:32 +0200

    added datalist

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> mountains = Arrays.asList("K2","Kebnekaise","Billingen", "Ida", "Hunneberg", "Halleberg");
+
+        recyclerView=findViewById(R.id.recycler_view);
+
     }
 }
